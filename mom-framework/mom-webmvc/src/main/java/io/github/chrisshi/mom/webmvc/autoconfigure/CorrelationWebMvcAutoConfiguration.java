09c076946738f6711c34f7a8d9835fbc9e5430f8
Chris Shi <christ.sxc@gmail.com>
2026-07-18 16:54:40 +0800

    fix(webmvc): condition correlation filtering on its own type

--- mom-framework/mom-webmvc/src/main/java/io/github/chrisshi/mom/webmvc/autoconfigure/CorrelationWebMvcAutoConfiguration.java
+++ mom-framework/mom-webmvc/src/main/java/io/github/chrisshi/mom/webmvc/autoconfigure/CorrelationWebMvcAutoConfiguration.java
@@ -4,21 +4,15 @@ import io.github.chrisshi.mom.webmvc.context.CorrelationIdFilter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
 
 @AutoConfiguration
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class CorrelationWebMvcAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean
-    FilterRegistrationBean<CorrelationIdFilter> correlationIdFilterRegistration() {
-        FilterRegistrationBean<CorrelationIdFilter> registration =
-                new FilterRegistrationBean<>(new CorrelationIdFilter());
-        registration.setName("momCorrelationIdFilter");
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE + 20);
-        return registration;
+    @ConditionalOnMissingBean(CorrelationIdFilter.class)
+    CorrelationIdFilter correlationIdFilter() {
+        return new CorrelationIdFilter();
     }
 }
