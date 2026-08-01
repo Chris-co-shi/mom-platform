4800f64009b038dbcd4de6d0d45f8fc99610aa50
Chris Shi <christ.sxc@gmail.com>
2026-08-01 19:01:16 +0800

    feat(cache): add cache type api

---
+++ mom-framework/mom-cache/src/main/java/io/github/chrisshi/mom/cache/api/CacheType.java
@@ -0,0 +1,17 @@
+package io.github.chrisshi.mom.cache.api;
+
+/**
+ * Cache categories managed by MOM cache infrastructure.
+ */
+public enum CacheType {
+
+    IAM_PERMISSION,
+
+    SYSTEM_DICTIONARY,
+
+    SYSTEM_PARAMETER,
+
+    SYSTEM_I18N,
+
+    USER_SESSION
+}
