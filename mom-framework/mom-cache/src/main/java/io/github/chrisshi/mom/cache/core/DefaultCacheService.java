46cfc18930da647c3da4e1370e5fd82a7f1c9737
Chris Shi <christ.sxc@gmail.com>
2026-08-01 20:05:01 +0800

    refactor(cache): route service through multilevel provider

--- mom-framework/mom-cache/src/main/java/io/github/chrisshi/mom/cache/core/DefaultCacheService.java
+++ mom-framework/mom-cache/src/main/java/io/github/chrisshi/mom/cache/core/DefaultCacheService.java
@@ -1,42 +1,35 @@
 package io.github.chrisshi.mom.cache.core;
 
+import io.github.chrisshi.mom.cache.api.CacheKey;
 import io.github.chrisshi.mom.cache.api.CachePolicy;
-import io.github.chrisshi.mom.cache.api.CacheProvider;
 import io.github.chrisshi.mom.cache.api.CacheService;
-import io.github.chrisshi.mom.cache.api.CacheType;
-
-import java.time.Duration;
 
 public class DefaultCacheService implements CacheService {
 
     private final CachePolicyRegistry policyRegistry;
-    private final CacheProviderSelector providerSelector;
+    private final MultiLevelCacheProvider cacheProvider;
 
     public DefaultCacheService(CachePolicyRegistry policyRegistry,
-                               CacheProviderSelector providerSelector) {
+                               MultiLevelCacheProvider cacheProvider) {
         this.policyRegistry = policyRegistry;
-        this.providerSelector = providerSelector;
+        this.cacheProvider = cacheProvider;
     }
 
     @Override
-    public <T> T get(CacheType type, String key, Class<T> clazz) {
-        CachePolicy policy = policyRegistry.get(type);
-        CacheProvider provider = providerSelector.select(policy);
-        Object value = provider.get(key);
-        return clazz.cast(value);
+    public <T> T get(CacheKey key, Class<T> clazz) {
+        CachePolicy policy = policyRegistry.get(key.type());
+        Object value = cacheProvider.get(key, policy);
+        return value == null ? null : clazz.cast(value);
     }
 
     @Override
-    public void put(CacheType type, String key, Object value) {
-        CachePolicy policy = policyRegistry.get(type);
-        CacheProvider provider = providerSelector.select(policy);
-        provider.put(key, value, policy.ttl());
+    public <T> void put(CacheKey key, T value) {
+        CachePolicy policy = policyRegistry.get(key.type());
+        throw new UnsupportedOperationException("Put operation will be completed by cache write coordinator");
     }
 
     @Override
-    public void evict(CacheType type, String key) {
-        CachePolicy policy = policyRegistry.get(type);
-        CacheProvider provider = providerSelector.select(policy);
-        provider.delete(key);
+    public void evict(CacheKey key) {
+        throw new UnsupportedOperationException("Evict operation will be completed by cache eviction coordinator");
     }
 }
