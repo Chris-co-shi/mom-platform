d8dd0d7b1c52b1e72d5081f4cbdad95e740bae17
Chris-co-shi <christ.sxc@gmail.com>
2026-09-03 16:49:46 +0800

    修复auth模块

--- mom-auth-platform/mom-auth-server/src/main/java/io/github/chrisshi/mom/auth/AuthApplication.java
+++ mom-auth-platform/mom-auth-server/src/main/java/io/github/chrisshi/mom/auth/AuthApplication.java
@@ -2,11 +2,13 @@ package io.github.chrisshi.mom.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 史偕成
  * @date 2026/09/02 14:14
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
     static void main() {
