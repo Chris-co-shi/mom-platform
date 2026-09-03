a8709dd99f2cab07001e835327b651e7423510a2
Chris-co-shi <christ.sxc@gmail.com>
2026-09-03 17:29:35 +0800

    修复auth模块

---
+++ mom-auth-platform/mom-auth-server/src/main/java/io/github/chrisshi/mom/auth/controller/TestController.java
@@ -0,0 +1,17 @@
+package io.github.chrisshi.mom.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 史偕成
+ * @date 2026/09/03 17:25
+ **/
+@RestController("/t")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello world";
+    }
+}
